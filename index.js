b851b145ab6e1425d8a274e11c275e1335caf3c3
Cefas Garcia Pereira <cefasgarciapereira@gmail.com>
2020-10-28 16:13:51 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { AppRegistry } from 'react-native';
+import { name as appName } from './app.json';
+import App from './src/App';
+
+export default function Main() {
+  return <App/>;
+}
+
+AppRegistry.registerComponent(appName, () => Main)
